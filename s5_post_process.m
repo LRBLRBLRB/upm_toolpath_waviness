3fd8b75d0f08beef01530a20660964e7277a1de5
LRBLRBLRB <550625844@qq.com>
2023-03-09 19:07:31 +0800

    20230309 post processing

--- s5_post_process.m
+++ s5_post_process.m
@@ -3,8 +3,19 @@
 isAPP = false;
 if isAPP
     workspaceDir = app.workspace;
-    spiralPath = app.spiralPath;
-    spiralNorm = app.spiralNorm;
+    if app.isSpiralFile
+        toolPathPath = fullfile(app.dirName,app.fileName);
+        % toolName = 'output_data\tool\toolTheo_3D.mat';
+        spiralPath = load(toolPathPath,'spiralPath');
+        spiralNorm = load(toolPathPath,'spiralNorm');
+        spiralCut = load(toolPathPath,'spiralCut');
+        spiralQuat = load(toolPathPath,'spiralQuat');
+    else
+        spiralPath = app.spiralPath;
+        spiralNorm = app.spiralNorm;
+        spiralCut = app.spiralCut;
+        spiralQuat = app.spiralQuat;
+    end
 else
     close all;
     addpath(genpath('funcs'));
@@ -21,23 +32,33 @@ else
         % toolName = 'output_data\tool\toolTheo_3D.mat';
         spiralPath = load(toolPathPath,'spiralPath');
         spiralNorm = load(toolPathPath,'spiralNorm');
+        spiralCut = load(toolPathPath,'spiralCut');
+        spiralQuat = load(toolPathPath,'spiralQuat');
     end
 end
 
-%% case 1: export apt file
+%% generate the 5-axis tool path from original data
+% toolpath should be saved in "x y z i j k" format
 
-[aptFname,aptPath,aptInd] = uiputfile( ...
-    {'*.apt','Automatically Programmed Tool(*.apt)';'*.*','All Files'}, ...
-    'Enter the file to save the tool path','toolPath.apt');
-aptFile = fullfile(aptPath,aptFname);
-aptFid = fopen(aptFile,'w');
-for ii = 1:size(spiralPath,2)
-    fprintf(aptFid,'GOTO/%f,%f,%f,%f,%f,%f\n', ...
-        spiralPath(1,ii),spiralPath(2,ii),spiralPath(3,ii), ...
-        spiralNorm(1,ii),spiralNorm(2,ii),spiralNorm(3,ii));
+
+
+switch postType
+    case 1
+        %% case 1: export apt file
+        [aptFname,aptPath,aptInd] = uiputfile( ...
+            {'*.apt','Automatically Programmed Tool(*.apt)';'*.*','All Files'}, ...
+            'Enter the file to save the tool path','toolPath.apt');
+        aptFile = fullfile(aptPath,aptFname);
+        aptFid = fopen(aptFile,'w');
+        for ii = 1:size(spiralPath,2)
+            fprintf(aptFid,'GOTO/%f,%f,%f,%f,%f,%f\n', ...
+                spiralPath(1,ii),spiralPath(2,ii),spiralPath(3,ii), ...
+                spiralNorm(1,ii),spiralNorm(2,ii),spiralNorm(3,ii));
+        end
+    case 2
+        %% case 2: post-processing to export the axial position
+        p
 end
-
-%% case 2: post-processing to export the axial position
 
 %%
 % rmpath(genpath('funcs'));
