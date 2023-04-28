af04a17822ded79ab60a4be8ba8a0c19714ab3d7
LRBLRBLRB <550625844@qq.com>
2023-04-28 11:24:12 +0800

    20230428 zygo surf fit

--- src/s7_zygo_surf_standard.m
+++ src/s7_zygo_surf_standard.m
@@ -1,7 +1,7 @@
 % The file is aimed to deal with the data measured by the Zygo white light interferometer, 
 % and regenerate the measured surface
 
-% close all;
+close all;
 clear;
 % clc;
 addpath(genpath('funcs'));
@@ -58,53 +58,40 @@ switch surfFileType
     case '.mat'
         % case for the mat data exported from datx files
         surfDataStruct = load(surfFilePath);
-        surfData = zeros([size(surfDataStruct.mh),3]);
-        [surfData(:,:,2),surfData(:,:,1)] = meshgrid(surfDataStruct.vx,surfDataStruct.vy);
-        surfData(:,:,3) = surfDataStruct.mh;
+        surfData0 = zeros([size(surfDataStruct.mh),3]);
+        [surfData0(:,:,2),surfData0(:,:,1)] = meshgrid(surfDataStruct.vx,surfDataStruct.vy);
+        surfData0(:,:,3) = surfDataStruct.mh;
         % unit convertion
         xyUnit0 = find(strcmp(unitList,surfDataStruct.XYunit),1); % default unit is milimeter
         zUnit0 = find(strcmp(unitList,surfDataStruct.Zunit),1); % default unit is meter
         aimUnit = find(strcmp(unitList,unit),1);
-        surfData(:,:,1:2) = 1000^(aimUnit - xyUnit0)*surfData(:,:,1:2);
-        surfData(:,:,3) = 1000^(aimUnit - zUnit0)*surfData(:,:,3);
+        surfData0(:,:,1:2) = 1000^(aimUnit - xyUnit0)*surfData0(:,:,1:2);
+        surfData0(:,:,3) = 1000^(aimUnit - zUnit0)*surfData0(:,:,3);
     case '.datx'
         % case for the .datx data
-        surfData
+        surfData0
     case '.xyz'
         % case for the .xyz data
         surfData0 = loadXYZ(surfFilePath,1000,1000,8.70983e-07,'um','um');
-        surfData(:,:,1) = surfData0.X;
-        surfData(:,:,2) = surfData0.Y;
-        surfData(:,:,3) = surfData0.Z;
+        surfData0(:,:,1) = surfData0.X;
+        surfData0(:,:,2) = surfData0.Y;
+        surfData0(:,:,3) = surfData0.Z;
 %         surfData = zygo_xyz(surfFilePath);
     case '.txt'
         % case for the txt data files
-        surfData = load(surfFilePath);
+        surfData0 = load(surfFilePath);
     otherwise
         return;
 end
 
-%         [surfFileName,surfDirName,surfFileType] = uigetfile({ ...
-%             '*.datx','data-files(*.datx)'; ...
-%             '*.xyz','xyz-files(*.xyz)'; ...
-%             '*.txt','text files(*.txt)'; ...
-%             '*.mat','MATLAB data files(*.mat)'; ...
-%             '*.*','all files(*.*)'}, ...
-%             'Select one zygo data file', ...
-%             fullfile(workspaceDir), ...
-%             'MultiSelect','off');
-%         surfFilePath1 = fullfile(surfDirName,surfFileName);
-%         surfDataStruct1 = load(surfFilePath1);
-%         [surfData1(:,:,1),surfData1(:,:,2)] = meshgrid(surfDataStruct1.vx,surfDataStruct1.vy);
-%         surfData1(:,:,3) = surfDataStruct1.mh;
-
-
 % plot the original result
 figure('Name','1 Zygo original result');
-surf( ...
-    surfData(:,:,1),surfData(:,:,2),surfData(:,:,3), ...
-    'FaceColor','flat','FaceAlpha',0.8,'LineStyle','none');
+surf(surfMesh(:,:,1),surfMesh(:,:,2),surfMesh(:,:,3), ...
+    'FaceColor',[0 0.4470 0.7410],'EdgeColor','none','FaceAlpha',0.1);
 hold on;
+surf( ...
+    surfData0(:,:,1),surfData0(:,:,2),surfData0(:,:,3), ...
+    'FaceColor','flat','FaceAlpha',0.8,'LineStyle','none');
 % surf( ...
 %     surfData1(:,:,1),surfData1(:,:,2),surfData1(:,:,3), ...
 %     'FaceColor','flat','FaceAlpha',0.8,'LineStyle','none');
@@ -114,56 +101,88 @@ ylabel(['y (',unit,')']);
 zlabel(['z (',unit,')']);
 
 %% point cloud registration
+% rough registration
+tmpZ = 0 - min(surfData0(:,:,3),[],'all');
+surfData0(:,:,3) = surfData0(:,:,3) + tmpZ;
+surfDataPt0 = reshape(surfData0,[],3);
+surfMeshPt = reshape(surfMesh,[],3);
+
 % using zyq's icp function
-% surfDataPt = reshape(surfData,[],3);
-% surfMeshPt = reshape(surfData,[],3);
-% [surfRot,surfTran] = icp_yq(surfDataPt,surfMeshPt);
+% surfDataPC0 = pointCloud(surfDataPt);
+% surfMeshPC0 = pointCloud(surfMeshPt);
+% gridStep = (surfDataPC0.XLimits(2) - surfDataPC0.XLimits(1))/25; % down sampling
+% surfDataPC = pcdownsample(surfDataPC0,'gridAverage',gridStep);
+% surfMeshPC = pcdownsample(surfMeshPC0,'gridAverage',gridStep);
+% [surfRot,surfTran] = icp_yq(surfDataPC.Location,surfMeshPC.Location);
 % surfDataPt = (surfRot*(surfDataPt.') + surfTran).';
-% surfData = reshape(surfDataPt,size(surfData,1),size(surfData,2),3);
+
 
 % using computer vision toolbox
-% pcregisterndt
-surfDataPC = pointCloud(surfData);
-surfMeshPC = pointCloud(surfMesh);
-% low-pass filter 
-gridStep = 0.1; % down sampling
-surfDataPC = pcdownsample(surfDataPC,'gridAverage',gridStep);
-surfMeshPC = pcdownsample(surfMeshPC,'gridAverage',gridStep);
-[surfRot,surfDataPC,rmse] = pcregistericp(surfDataPC,surfMeshPC);
-
+% % out liers removing
+% surfDataPt = rmoutliers(surfDataPt,'median');
+% % low-pass filter
+% % surfDataPt
+% 
+% figure; plot3(surfDataPt(:,1),surfDataPt(:,2),surfDataPt(:,3),'.', ...
+%     'Color',[0.8500 0.3250 0.0980],'MarkerSize',1);
+% 
+% surfDataPC0 = pointCloud(surfDataPt);
+% surfMeshPC = pointCloud(surfMeshPt);
+% 
+% 
+% gridStep1 = (surfDataPC0.XLimits(2) - surfDataPC0.XLimits(1))/25; % down sampling
+% [surfHomo1,surfDataPC1,rmse1] = pcregisterndt(surfDataPC0,surfMeshPC,gridStep1);
+% gridStep2 = (surfDataPC1.XLimits(2) - surfDataPC1.XLimits(1))/200; % down sampling
+% surfDataPC2 = pcdownsample(surfDataPC1,'gridAverage',gridStep2);
+% surfMeshPC = pcdownsample(surfMeshPC,'gridAverage',gridStep2);
+% [surfHomo2,surfDataPC3,rmse2] = pcregistericp(surfDataPC2,surfMeshPC);
+% surfDataPt = reshape(surfData,[],3);
+% surfDataPt = (surfDataPt*surfHomo1.Rotation + surfHomo1.Translation) ...
+%     *surfHomo2.Rotation + surfHomo2.Translation;
 
 % manual registration
-% surfDataPt = reshape(surfData,[],3);
-% surfMeshPt = reshape(surfData,[],3);
-% surfRot = eye(3,3);
-% tmpZ = 0 - min(surfDataPt(:,3));
-% surfTran = [0;0;tmpZ];
-% surfDataPt = (surfRot*(surfDataPt.') + surfTran).';
-% surfData = reshape(surfDataPt,size(surfData,1),size(surfData,2),3);
+surfRot = eul2rotm([0,pi/900,0],'ZYX');
+surfTran = [0;0;0];
+surfDataPt1 = (surfRot*(surfDataPt0') + surfTran)';
 
+% get the registered measured surface
+surfData1 = reshape(surfDataPt1,size(surfData0,1),size(surfData0,2),3);
 
 figure('Name','2 measured surface and designed surface');
-surf(surfData(:,:,1),surfData(:,:,2),surfData(:,:,3), ...
+surf(surfData1(:,:,1),surfData1(:,:,2),surfData1(:,:,3), ...
     'FaceColor',[0.8500 0.3250 0.0980],'EdgeColor','none','FaceAlpha',0.1);
 hold on;
 surf(surfMesh(:,:,1),surfMesh(:,:,2),surfMesh(:,:,3), ...
     'FaceColor',[0 0.4470 0.7410],'EdgeColor','none','FaceAlpha',0.1);
-
+xlabel(['x (',unit,')']);
+ylabel(['y (',unit,')']);
+zlabel(['z (',unit,')']);
 
 
 %% fit the theoretical surface
 % z = A.*((x - x0).^2 + (y - y0).^2)./2 + C + z0;
+surfDataPt1 = reshape(surfData0,[],3);
 
+% least-square result of linear equations
+% coeffMat = [surfDataPt1(:,1).^2 + surfDataPt1(:,2).^2,ones(size(surfDataPt1,1),1)];
+% reMat = surfDataPt1(:,3);
+% fitResult = (coeffMat'*coeffMat)\(coeffMat'*reMat);
 
-
+% lsqcurvefit
+f = @(x,xdata) x(1)/2*(xdata(:,1).^2 + xdata(:,2).^2) + x(2);
+x = lsqcurvefit(f,[A,C],surfDataPt1(:,1:2),surfDataPt1(:,3));
 
 %% surface error calculation
-deltaZ = surfData(:,:,3) - surfFunc(surfData(:,:,1),surfData(:,:,2));
+deltaZ = surfData1(:,:,3) - surfFunc(surfData1(:,:,1),surfData1(:,:,2));
 Sa = mean(abs(deltaZ));
-Sz
-Sq = max(deltaZ,[],'all') - min(deltaZ,[],'all');
+Sz = max(deltaZ,[],'all') - min(deltaZ,[],'all');
+Sq = sqrt(mean(deltaZ.^2));
 
-
+figure('Name','3 - surface error');
+surf(surfData1(:,:,1),surfData1(:,:,2),deltaZ,'EdgeColor','none');
+xlabel(['x (',unit,')']);
+ylabel(['y (',unit,')']);
+zlabel(['\Deltaz (',unit,')']);
 
 %%
 % rmpath(genpath('funcs'));
